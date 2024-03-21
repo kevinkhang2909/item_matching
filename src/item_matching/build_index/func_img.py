a0f9749f890b3d3611402bf9c9eee1f6067d730c
kevin <kevinkhang2909@gmail.com>
2024-03-21 13:59:18 +0700

    match by image pipeline 0.0.13

--- src/item_matching/build_index/func_img.py
+++ src/item_matching/build_index/func_img.py
@@ -4,7 +4,7 @@ import polars as pl
 from core_pro.ultilities import make_dir
 import sys
 from loguru import logger
-from func import clean_text
+from item_matching.build_index.func import clean_text
 
 logger.remove()
 logger.add(sys.stdout, colorize=True, format='<green>{time:HH:mm:ss}</green> | <level>{level}</level> | <cyan>{function}</cyan> | <level>{message}</level>')
