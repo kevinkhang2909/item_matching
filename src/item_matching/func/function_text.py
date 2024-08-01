2a14fb9a5bca470749e6ba04d4d9463dee954a6f
kevinkhang <kevinkhang2909@gmail.com>
2024-08-01 15:54:59 +0700

    clean text 0.0.52

--- src/item_matching/func/function_text.py
+++ src/item_matching/func/function_text.py
@@ -2,8 +2,9 @@ import polars as pl
 import duckdb
 import sys
 import re
+import emoji
+from tqdm import tqdm
 from loguru import logger
-
 logger.remove()
 logger.add(sys.stdout, colorize=True, format='<level>{level}</level> | <cyan>{function}</cyan> | <level>{message}</level>')
 
@@ -13,14 +14,50 @@ class PipelineText:
         self.mode = mode
 
     @staticmethod
-    def clean_text(data: pl.DataFrame, col: str = 'item_name') -> pl.DataFrame:
-        regex = "[\(\[\<\"].*?[\)\]\>\"]"
-        return data.with_columns(
-            pl.col(col).map_elements(
-                lambda x: re.sub(regex, "", x).lower().rstrip('.').strip(), return_dtype=pl.String
-            )
-            .alias(f'{col.lower()}_clean')
+    def remove_text_between_emojis(text):
+        # regex pattern to match emojis
+        emoji_pattern = re.compile(
+            "["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            u"\U00002702-\U000027B0"
+            u"\U000024C2-\U0001F251"
+            "]+",
+            flags=re.UNICODE
         )
+        # find all emojis in the text
+        emojis = emoji_pattern.findall(text)
+        # if there are less than 2 emojis, return the original text
+        if len(emojis) < 2:
+            return text
+        else:
+            regex = f"[{emojis[0]}].*?[{emojis[1]}]"
+            return re.sub(regex, "", text)
+
+    @staticmethod
+    def clean_text_pipeline(text: str) -> str:
+        regex = r"[\(\[\<\"\|].*?[\)\]\>\"\|]"
+        text = text.lower().strip()
+        text = PipelineText.remove_text_between_emojis(text)
+        text = emoji.replace_emoji(text, ' ')
+        text = re.sub(regex, ' ', text)
+        text = re.sub(r'\-|\_|\*', ' ', text)
+        text = re.sub(r'\s+', ' ', text)
+        return text.rstrip('.').strip()
+
+    @staticmethod
+    def clean_text(data: pl.DataFrame, col: str = 'item_name') -> pl.DataFrame:
+        lst = [PipelineText.clean_text_pipeline(x) for x in tqdm(data[col].to_list())]
+        return data.with_columns(pl.Series(name=f'{col}_clean', values=lst))
+        # regex = "[\(\[\<\"].*?[\)\]\>\"]"
+        # return data.with_columns(
+        #     pl.col(col).map_elements(
+        #         lambda x: re.sub(regex, "", x).lower().rstrip('.').strip(), return_dtype=pl.String
+        #     )
+        #     .alias(f'{col.lower()}_clean')
+        # )
 
     def run(self, data, key_col: list = None):
         # load data
