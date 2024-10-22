f7c40fa8a7723b8b5ac6540a62837b98b55228e8
kevin <kevinkhang2909@gmail.com>
2024-10-22 10:46:05 +0700

    add rerank 0.0.71

--- src/item_matching/__init__.py
+++ src/item_matching/__init__.py
@@ -2,5 +2,5 @@ from .main_2 import ModelInput, PipelineMatch
 from .main_1 import ModelInputInner, PipelineMatchInner
 from .func.function_image import PipelineImage
 from .func.function_text import PipelineText
-from .func.post_processing import PostProcessing, save_parquet_to_csv
+from .func.post_processing import PostProcessing
 from .pipeline.rerank import ReRank, ReRankConfig
--- src/item_matching/func/post_processing.py
+++ src/item_matching/func/post_processing.py
@@ -4,19 +4,21 @@ from .utilities import make_dir
 
 
 class PostProcessing:
-    def __init__(self, data):
+    def __init__(self, data, verbose: bool = False):
         self.data = data
+        self.verbose = verbose
         self.final_col = None
         self.query_add_url_duckdb = ''
         self.query_add_show_img = ''
         self.mode = ['db', 'q']
 
-    def select_export_cols(self):
-        q_col = sorted(filter(lambda x: 'q' in x and 'image_url' not in x, self.data.columns))
-        db_col = sorted(filter(lambda x: 'db' in x and 'image_url' not in x, self.data.columns))
-        score_col = sorted(filter(lambda x: 'score' in x, self.data.columns))
-        self.final_col = q_col + db_col + ['q_image_url', 'db_image_url'] + score_col
-        return self.final_col
+    @staticmethod
+    def select_export_cols(data):
+        q_col = sorted(filter(lambda x: 'q' in x and 'image_url' not in x, data.columns))
+        db_col = sorted(filter(lambda x: 'db' in x and 'image_url' not in x, data.columns))
+        score_col = sorted(filter(lambda x: 'score' in x, data.columns))
+        final_col = q_col + db_col + ['q_image_url', 'db_image_url'] + score_col
+        return final_col
 
     def add_url(self):
         for i in self.mode:
@@ -31,23 +33,33 @@ class PostProcessing:
         return self.query_add_show_img
 
     def run(self):
+        # init
+        show_image = self.add_show_image()
+        add_exclude = ''
+        if len(show_image) != 0:
+            add_exclude += 'EXCLUDE (db_image_url, q_image_url)'
         data = self.data
+
+        # query
         query = f"""
-        select *
+        select * {add_exclude}
         {self.add_show_image()}
         {self.add_url()}
         from data
         """
-        df = (
-            duckdb.sql(query).pl()
-            .select(self.select_export_cols())
-        )
+        if self.verbose:
+            print(query)
+        else:
+            df = duckdb.sql(query).pl()
+            select_col = PostProcessing.select_export_cols(df)
+            print(select_col)
+            df = df.select(select_col)
         return df
 
-
-def save_parquet_to_csv(path: Path):
-    path_export = path.parent / 'result_export'
-    make_dir(path_export)
-    for f in [*path.glob('*.parquet')]:
-        query = f"""COPY (SELECT * FROM read_parquet('{str(f)}')) TO '{path_export}/{f.stem}.csv' (HEADER, DELIMITER ',')"""
-        duckdb.sql(query)
+    @staticmethod
+    def save_parquet_to_csv(path: Path):
+        path_export = path.parent / 'result_export'
+        make_dir(path_export)
+        for f in [*path.glob('*.parquet')]:
+            query = f"""COPY (SELECT * FROM read_parquet('{str(f)}')) TO '{path_export}/{f.stem}.csv' (HEADER, DELIMITER ',')"""
+            duckdb.sql(query)
