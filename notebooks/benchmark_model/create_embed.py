c769661bfe30e7bde71f0972feacdb72bc5c42c4
kevin <kevinkhang2909@gmail.com>
2024-11-25 17:43:31 +0700

    clean codes

--- notebooks/benchmark_model/create_embed.py
+++ notebooks/benchmark_model/create_embed.py
@@ -1,6 +1,4 @@
 import duckdb
-from re import search
-import polars as pl
 from core_pro.ultilities import make_sync_folder
 from pathlib import Path
 import sys
@@ -13,56 +11,56 @@ from src.item_matching.pipeline.rerank import ReRank, ReRankConfig
 
 
 # path
-cluster = 'Fashion'
+cluster = 'FMCG'
 path = make_sync_folder('Item_Matching_Test')
 
 file = path / f'data_sample_{cluster}_0.parquet'
 file_inner = path / 'inner.parquet'
 
 # data loading
-# query = f"""
-# with base as (
-# select * exclude(level1_global_be_category, level2_global_be_category, level3_global_be_category)
-# , level1_global_be_category || '__' || level2_global_be_category || '__' || level3_global_be_category category
-# from read_parquet('{file}')
-# )
-# select *
-# from base
-# """
-# df = (
-#     duckdb.sql(query).pl()
-#     .unique(['item_id'])
-# )
-#
-# # text
-# df = PipelineText().run(df)
-# df.write_parquet(file_inner)
-#
-# config = MatchInput(
-#     ROOT_PATH=path,
-#     PATH_INNER=file_inner,
-#     INNER=True ,
-#     MATCH_BY='text',
-#     COL_CATEGORY='category',
-#     TOP_K=10,
-#     EXPLODE=False,
-# )
-# PipelineMatch(config).run()
-#
-# # image
-# df, df_img = PipelineImage(path / f'download_img_{cluster}').run(df)
-# df.write_parquet(file_inner)
-#
-# config = MatchInput(
-#     ROOT_PATH=path,
-#     PATH_INNER=file_inner,
-#     INNER=True ,
-#     MATCH_BY='image',
-#     COL_CATEGORY='category',
-#     TOP_K=10,
-#     EXPLODE=False,
-# )
-# PipelineMatch(config).run()
+query = f"""
+with base as (
+select * exclude(level1_global_be_category, level2_global_be_category, level3_global_be_category)
+, level1_global_be_category || '__' || level2_global_be_category || '__' || level3_global_be_category category
+from read_parquet('{file}')
+)
+select *
+from base
+"""
+df = (
+    duckdb.sql(query).pl()
+    .unique(['item_id'])
+)
+
+# text
+df = PipelineText().run(df)
+df.write_parquet(file_inner)
+
+config = MatchInput(
+    ROOT_PATH=path,
+    PATH_INNER=file_inner,
+    INNER=True ,
+    MATCH_BY='text',
+    COL_CATEGORY='category',
+    TOP_K=10,
+    EXPLODE=False,
+)
+PipelineMatch(config).run()
+
+# image
+df, df_img = PipelineImage(path / f'download_img_{cluster}').run(df)
+df.write_parquet(file_inner)
+
+config = MatchInput(
+    ROOT_PATH=path,
+    PATH_INNER=file_inner,
+    INNER=True ,
+    MATCH_BY='image',
+    COL_CATEGORY='category',
+    TOP_K=10,
+    EXPLODE=False,
+)
+PipelineMatch(config).run()
 
 # rerank
 config = ReRankConfig(ROOTPATH=path, EXPLODE=True)
