9bf30ffceed19890d9c0635f729e6b0501fdf523
kevin <kevinkhang2909@gmail.com>
2024-12-30 16:35:06 +0700

    fix rerank 0.0.85

--- src/item_matching/pipeline/rerank.py
+++ src/item_matching/pipeline/rerank.py
@@ -11,6 +11,8 @@ from ..func.post_processing import PostProcessing, data_explode_list
 class ReRankConfig(BaseModel):
     ROOTPATH: Path = Field(default=None)
     EXPLODE: bool = Field(default=False)
+    col_text: str = Field(default='')
+    col_image: str = Field(default='')
 
     @computed_field
     @property
@@ -57,34 +59,41 @@ class ReRank:
         self.file_text = record.file_text
         self.file_image = record.file_image
 
+        # col
+        self.col_text = record.col_text
+        self.col_image = record.col_image
+
     def _data_check(self, category: str):
         paths = {'text': self.path_text, 'image': self.path_image}
-        df_dict, total_cols = {}, []
+        df_dict = {}
         for name, path in paths.items():
             df = pl.read_parquet(path / f'{category}.parquet')
             if self.EXPLODE:
                 df = data_explode_list(df)
-            total_cols.extend(df.columns)
             df_dict[name] = df
-        total_cols = set(total_cols)
+        return df_dict
 
-        # select cols
-        patterns = ['item_name_clean', 'file_path', 'rnk', 'exists', 'score_text_embed', 'score_image_embed']
-        select_cols = [col for col in total_cols if not any(pattern in col for pattern in patterns)]
-        duckdb_cols = '\n, '.join([f'{i}' for i in select_cols])
-        return df_dict, duckdb_cols
-
-    def rerank_score(self, data_text, data_image, duckdb_cols):
+    def rerank_score(self, data_text, data_image):
         # query
         query = f"""
             -- combine text & image
             with base as (
-                select {duckdb_cols}
+                select q_index
+                , q_{self.col_text}
+                , db_index
+                , db_{self.col_text}
+                , q_level1_global_be_category level1_global_be_category
                 , 'score_text' match_type
                 , score_text_embed score
                 from data_text
                 union all
-                select {duckdb_cols}
+                select q_index
+                , q_{self.col_text}
+                , q_{self.col_image}
+                , db_{self.col_image}
+                , db_index
+                , db_{self.col_text}
+                , q_level1_global_be_category level1_global_be_category
                 , 'score_image' match_type
                 , score_image_embed score
                 from data_image
@@ -94,13 +103,12 @@ class ReRank:
                 PIVOT base
                 ON match_type
                 USING sum(score)
-                GROUP BY {duckdb_cols}
+                GROUP BY q_index, q_{self.col_image}, db_{self.col_image}, db_index, db_{self.col_text}, level1_global_be_category
             )
             -- calculate mean, max rerank
             , cal_tab as (
                 select *
                 , (coalesce(score_text, 0) + coalesce(score_image, 0)) / 2 as score_mean
---                 , greatest(score_text, score_image) as score_max
                 from pivot_tab
                 where db_item_id != q_item_id
             )
@@ -114,8 +122,8 @@ class ReRank:
     def run(self):
         total_cat = len(self.all_category)
         for cat in tqdm(self.all_category, total=total_cat, desc='ReRanking'):
-            df_dict, duckdb_cols = self._data_check(cat)
-            df = self.rerank_score(data_text=df_dict['text'], data_image=df_dict['image'], duckdb_cols=duckdb_cols)
+            df_dict = self._data_check(cat)
+            df = self.rerank_score(data_text=df_dict['text'], data_image=df_dict['image'])
             df = PostProcessing(df).run()
             df.write_parquet(self.path_result / f'{cat}.parquet')
         print(f'[RERANK]: Done {total_cat} categories')
